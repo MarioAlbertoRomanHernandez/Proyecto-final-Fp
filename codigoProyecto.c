a42044a021db0b7e3265b22870e4210fff15aea7
Mario Alberto <albertomario1708@gmail.com>
2024-11-20 20:36:41 -0600

    agrege los de las promociones, modifique el menu pero sigo sin saber los de archivos

--- codigoProyecto.c
+++ codigoProyecto.c
@@ -1,42 +1,43 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
+#include <time.h>
 
 void mostrarSucursales();
 void mostrarMenu();
 void mostrarBebidasCalientes();
 void mostrarBebidasFrias();
-void mostrarComidas();
+void mostrarSnacks();
 void agregarAlCarrito(char *producto, float precio, int calorias);
 void mostrarCarrito();
 void aplicarDescuento(float *total);
 void pagar();
+void iniciarSesion();
+void registrarUsuario();
+int verificarUsuario(char *usuario, char *contrasena);
 
-char *sucursales[] = {"Sucursal 1", "Sucursal 2", "Sucursal 3"};
-char *direcciones[] = {"Cerca de CU, Dirección 1", "Cerca de CU, Dirección 2", "Cerca de CU, Dirección 3"};
-
-char *bebidasCalientes[] = {"Café Americano", "Latte", "Capuchino", "Té Verde Matcha Latte", "Infusión de Hierbas (manzanilla, menta, jengibre)"};
+char *sucursales[] = {"Facultad de Ingenieria", "Facultad de Medicina"};
+char *direcciones[] = {"Cerca de CU, Facultad de Ingenieria", "Cerca de CU, Facultad de Medicina"};
+char *bebidasCalientes[] = {"Americano", "Latte", "Capuchino", "Te Verde", "Infusion"};
 float preciosBebidasCalientes[] = {30.0, 40.0, 45.0, 50.0, 25.0};
 int caloriasBebidasCalientes[] = {5, 150, 120, 100, 0};
-
-char *bebidasFrias[] = {"Frappé de Chocolate", "Frappé de Matcha", "Frappé de Café Clásico"};
-float preciosBebidasFrias[] = {55.0, 60.0, 50.0};
-int caloriasBebidasFrias[] = {300, 250, 200};
-
-char *comidas[] = {"Sandwich", "Ensalada", "Panini"};
-float preciosComidas[] = {50.0, 45.0, 55.0};
-int caloriasComidas[] = {300, 200, 350};
-
+char *bebidasFrias[] = {"Americano Frio", "Frappe de Chocolate", "Frappe de Matcha", "Frappe de Cafe Clasico", "Frappe de Moka"};
+float preciosBebidasFrias[] = {45.0, 55.0, 60.0, 50.0, 65.0};
+int caloriasBebidasFrias[] = {15, 300, 250, 200, 280};
+char *snacks[] = {"Yogurt con Fruta y Granola", "Ensalada", "Wrap Integral", "Barrita de Avena y Frutos Secos", "Fruta"};
+float preciosSnacks[] = {50.0, 60.0, 75.0, 15.0, 45.0};
+int caloriasSnacks[] = {310, 130, 250, 220, 50};
 char *carritoProductos[50];
 float carritoPrecios[50];
 int carritoCalorias[50];
-int carritoCantidad = 0;
+int carritoCantidad = 0, i;
 
 int main() {
     int opcion, sucursalSeleccionada, productoSeleccionado;
+    iniciarSesion();
     mostrarSucursales();
     scanf("%d", &sucursalSeleccionada);
     printf("Bienvenido a la %s\n", sucursales[sucursalSeleccionada - 1]);
-
     do {
         mostrarMenu();
         scanf("%d", &opcion);
@@ -45,23 +46,35 @@ int main() {
                 mostrarBebidasCalientes();
                 printf("Seleccione una bebida caliente para agregar al carrito:\n");
                 scanf("%d", &productoSeleccionado);
-                agregarAlCarrito(bebidasCalientes[productoSeleccionado - 1], preciosBebidasCalientes[productoSeleccionado - 1], caloriasBebidasCalientes[productoSeleccionado - 1]);
+                if(productoSeleccionado > 0 && productoSeleccionado <= 5){
+                    agregarAlCarrito(bebidasCalientes[productoSeleccionado - 1], preciosBebidasCalientes[productoSeleccionado - 1], caloriasBebidasCalientes[productoSeleccionado - 1]);
+                } else {
+                    printf("Opcion no valida\n");
+                }
                 break;
             case 2:
                 mostrarBebidasFrias();
-                printf("Seleccione una bebida fría para agregar al carrito:\n");
+                printf("Seleccione una bebida fria para agregar al carrito:\n");
                 scanf("%d", &productoSeleccionado);
-                agregarAlCarrito(bebidasFrias[productoSeleccionado - 1], preciosBebidasFrias[productoSeleccionado - 1], caloriasBebidasFrias[productoSeleccionado - 1]);
+                if(productoSeleccionado > 0 && productoSeleccionado <= 5){
+                    agregarAlCarrito(bebidasFrias[productoSeleccionado - 1], preciosBebidasFrias[productoSeleccionado - 1], caloriasBebidasFrias[productoSeleccionado - 1]);
+                } else {
+                    printf("Opcion no valida\n");
+                }
                 break;
             case 3:
-                mostrarComidas();
-                printf("Seleccione una comida para agregar al carrito:\n");
+                mostrarSnacks();
+                printf("Seleccione un snack para agregar al carrito:\n");
                 scanf("%d", &productoSeleccionado);
-                agregarAlCarrito(comidas[productoSeleccionado - 1], preciosComidas[productoSeleccionado - 1], caloriasComidas[productoSeleccionado - 1]);
+                if(productoSeleccionado > 0 && productoSeleccionado <= 5){
+                    agregarAlCarrito(snacks[productoSeleccionado - 1], preciosSnacks[productoSeleccionado - 1], caloriasSnacks[productoSeleccionado - 1]);
+                } else {
+                    printf("Opcion no valida\n");
+                }
                 break;
             case 4:
                 mostrarCarrito();
-                printf("¿Desea proceder al pago? (1: Sí, 0: No): ");
+                printf("¿Desea proceder al pago? (1: Si, 0: No): ");
                 int proceder;
                 scanf("%d", &proceder);
                 if (proceder) {
@@ -70,55 +83,55 @@ int main() {
                 break;
             case 5:
                 printf("Contactar servicio:\n");
-                printf("CEO: [Por agregar]\n");
-                printf("Atención al cliente: [Por agregar]\n");
+                printf("Sucursal F. Ingenieria: +52 55 1234 5678\n");
+                printf("Sucursal F. Medicina: +52 55 8765 4321\n");
+                printf("Atención al cliente: soporte@cafesync.com\n");
                 break;
             case 6:
-                printf("Gracias por visitar nuestra cafetería. ¡Hasta luego!\n");
+                printf("Gracias por visitar nuestra cafeteria. ¡Hasta luego! :D\n");
                 break;
             default:
-                printf("Opción no válida. Intente de nuevo.\n");
+                printf("Opcion no valida. Intente de nuevo.\n");
         }
     } while (opcion != 6);
-
     return 0;
 }
 
 void mostrarSucursales() {
     printf("Seleccione una sucursal:\n");
-    for (int i = 0; i < 3; i++) {
+    for(i = 0; i < 2; i++) {
         printf("%d. %s - %s\n", i + 1, sucursales[i], direcciones[i]);
     }
 }
 
 void mostrarMenu() {
-    printf("Menú:\n");
+    printf("Menu:\n");
     printf("1. Bebidas Calientes\n");
-    printf("2. Bebidas Frías\n");
-    printf("3. Comidas\n");
+    printf("2. Bebidas Frias\n");
+    printf("3. Snacks\n");
     printf("4. Ver carrito\n");
     printf("5. Contactar servicio\n");
     printf("6. Salir\n");
 }
 
 void mostrarBebidasCalientes() {
-    printf("Menú de Bebidas Calientes:\n");
-    for (int i = 0; i < 5; i++) {
-        printf("%d. %s - $%.2f - %d calorías\n", i + 1, bebidasCalientes[i], preciosBebidasCalientes[i], caloriasBebidasCalientes[i]);
+    printf("Menu de Bebidas Calientes:\n");
+    for (i = 0; i < 5; i++) {
+        printf("%d. %s - $%.2f - %d calorias\n", i + 1, bebidasCalientes[i], preciosBebidasCalientes[i], caloriasBebidasCalientes[i]);
     }
 }
 
 void mostrarBebidasFrias() {
-    printf("Menú de Bebidas Frías:\n");
-    for (int i = 0; i < 3; i++) {
-        printf("%d. %s - $%.2f - %d calorías\n", i + 1, bebidasFrias[i], preciosBebidasFrias[i], caloriasBebidasFrias[i]);
+    printf("Menu de Bebidas Frias:\n");
+    for (i = 0; i < 5; i++) {
+        printf("%d. %s - $%.2f - %d calorias\n", i + 1, bebidasFrias[i], preciosBebidasFrias[i], caloriasBebidasFrias[i]);
     }
 }
 
-void mostrarComidas() {
-    printf("Menú de Comidas:\n");
-    for (int i = 0; i < 3; i++) {
-        printf("%d. %s - $%.2f - %d calorías\n", i + 1, comidas[i], preciosComidas[i], caloriasComidas[i]);
+void mostrarSnacks() {
+    printf("Menu de Snacks:\n");
+    for (i = 0; i < 5; i++) {
+        printf("%d. %s - $%.2f - %d calorias\n", i + 1, snacks[i], preciosSnacks[i], caloriasSnacks[i]);
     }
 }
 
@@ -133,34 +146,61 @@ void mostrarCarrito() {
     printf("Carrito de Compras:\n");
     float total = 0;
     int totalCalorias = 0;
-    for (int i = 0; i < carritoCantidad; i++) {
-        printf("%d. %s - $%.2f - %d calorías\n", i + 1, carritoProductos[i], carritoPrecios[i], carritoCalorias[i]);
+    for (i = 0; i < carritoCantidad; i++) {
+        printf("%s - $%.2f - %d calorias\n", carritoProductos[i], carritoPrecios[i], carritoCalorias[i]);
         total += carritoPrecios[i];
         totalCalorias += carritoCalorias[i];
     }
     printf("Total: $%.2f\n", total);
-    printf("Total de calorías: %d\n", totalCalorias);
+    printf("Total de Calorias: %d\n", totalCalorias);
+    aplicarDescuento(&total);
+    printf("Total con descuento: $%.2f\n", total);
 }
 
 void aplicarDescuento(float *total) {
-    char numeroCuenta[10];
-    printf("Ingrese su número de cuenta de la UNAM (9 dígitos): ");
-    scanf("%s", numeroCuenta);
-    if (strlen(numeroCuenta) == 9) {
-        *total *= 0.9; // Aplica un 10% de descuento
-        printf("Descuento aplicado. Nuevo total: $%.2f\n", *total);
+    time_t t = time(NULL);
+    struct tm tm = *localtime(&t);
+    if (tm.tm_mon == 10 || tm.tm_mon == 5) { // Noviembre o Junio
+        printf("Descuento del 20%% aplicado por semana de examenes.\n");
+        *total *= 0.80;
     } else {
-        printf("Número de cuenta inválido. No se aplicó el descuento.\n");
+        printf("Descuento del 15%% aplicado por ser de la UNAM.\n");
+        *total *= 0.85;
+    }
+    if (tm.tm_hour < 10) { // Antes de las 10 AM
+        printf("Descuento del 10%% adicional por combo desayuno.\n");
+        *total *= 0.90;
     }
 }
 
 void pagar() {
-    float total = 0;
-    for (int i = 0; i < carritoCantidad; i++) {
-        total += carritoPrecios[i];
-    }
-    printf("Total a pagar: $%.2f\n", total);
-    aplicarDescuento(&total);
-    printf("Pago realizado. Gracias por su compra.\n");
-    carritoCantidad = 0; // Vaciar el carrito después de pagar
+    printf("Pago realizado con exito. ¡Gracias por su compra!\n");
 }
+
+void iniciarSesion() {
+    int opcion;
+    printf("1. Iniciar Sesion\n");
+    printf("2. Registrarse\n");
+    scanf("%d", &opcion);
+    if (opcion == 1) {
+        char usuario[50], contrasena[50];
+        printf("Usuario: ");
+        scanf("%s", usuario);
+        printf("Contrasena: ");
+        scanf("%s", contrasena);
+        if (verificarUsuario(usuario, contrasena)) {
+            printf("Inicio de sesion exitoso.\n");
+        } else {
+            printf("Usuario o contrasena incorrectos. Intente de nuevo.\n");
+            iniciarSesion();
+        }
+    } else if (opcion == 2) {
+        registrarUsuario();
+    } else {
+        printf("Opcion no valida. Intente de nuevo.\n");
+        iniciarSesion();
+    }
+}
+
+void registrarUsuario() {
+    char usuario[50], contras
